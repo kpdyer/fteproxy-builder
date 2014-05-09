32af37c565a09fb0105c073d3837fe3146482f02
Kevin P. Dyer <kpdyer@gmail.com>
2014-05-09 16:57:55 -0700

    update to use pip to install python modules

--- build/gnulinux-i386/build_fteproxy.sh
+++ build/gnulinux-i386/build_fteproxy.sh
@@ -30,17 +30,17 @@ sudo apt-get -y --no-install-recommends install build-essential
 sudo apt-get -y --no-install-recommends install upx
 sudo apt-get -y --no-install-recommends install git-core
 sudo apt-get -y --no-install-recommends install python-dev
+sudo apt-get -y --no-install-recommends install python-pip
 sudo apt-get -y --no-install-recommends install libgmp-dev
 sudo apt-get -y --no-install-recommends install libyaml-dev
-sudo apt-get -y --no-install-recommends install python-pip
-sudo apt-get -y --no-install-recommends install python-twisted
-sudo apt-get -y --no-install-recommends install python-crypto
-sudo apt-get -y --no-install-recommends install python-pyptlib
-sudo apt-get -y --no-install-recommends install obfsproxy
 sudo apt-get -y --no-install-recommends install debhelper
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -f
 
 sudo pip install --upgrade pip
+sudo pip install --upgrade twisted
+sudo pip install --upgrade crypto
+sudo pip install --upgrade pyptlib
+sudo pip install --upgrade obfsproxy
 sudo pip install --upgrade pyinstaller
 sudo pip install --upgrade fte
 
